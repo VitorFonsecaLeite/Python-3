b49d3f7d2dd5fff4439e5d8d0cfe8be23bb78749
Vitor Fonseca <vitor1712work@gmail.com>
2024-07-31 19:40:00 -0300

    Update 20-sorteio_em_ordem.py

--- Orientação a objetos/exercicos/20-sorteio_em_ordem.py
+++ Orientação a objetos/exercicos/20-sorteio_em_ordem.py
@@ -1,10 +1,10 @@
 import random
-cont=1
-alunos=[]
-while cont<=4 :
-    n=str(input(f"digite o nome do {cont}ºaluno :"))
+cont = 1
+alunos = []
+while cont <= 4:
+    n = str(input(f"digite o nome do {cont}ºaluno :"))
     alunos.append(n)
-    cont=cont+1
+    cont = cont+1
 random.shuffle(alunos)
 print("A ordem de apresentação seras :")
 print(alunos)
